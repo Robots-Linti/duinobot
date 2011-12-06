862a60a463f8e182447aff835f85a82f5c939711
jwackito@venabili <jbogado@linti.unlp.edu.ar>
2011-12-06 12:38:37 -0300

    Agregado robot.py
    Implementa las clases Board y Robot

---
+++ robot.py
@@ -0,0 +1,183 @@
+#!/bin/python
+# -*- coding: utf-8 -*-
+###############################################################################
+# duinobot-interactive
+# Conjunto de clases python para permitir que el robot Multiplo Duinobot N6
+# pueda programarse en forma interactiva, sin necesidad de bajar firmware al
+# robot. Sin embargo es necesario el soporte del firmware del lado del robot
+#
+# Copyright (C) 2011 David Vilaseca <http://www.robotgroup.com.ar>
+# Copyright (C) 2011 Joaquín Bogado <jbogado en linti.unlp.edu.ar>
+# 
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the RobotGroup Multiplo Pacifist License (RMPL)
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the RMPL license along with this program. 
+# If not, see <multiplo.com.ar/soft/Mbq/Lic.Minibloq.ESP.pdf>.
+###############################################################################
+
+from pyfirmata import DuinoBot, util
+
+class Board:
+    def __init__(self, device='/dev/ttyUSB0'):
+        '''Inicializa el dispositivo de conexion con el/los robot/s'''
+        self.board = DuinoBot(device)
+        it = util.Iterator(self.board)
+        it.start()
+        self.board.pass_time(0.1)
+
+    def __del__(self):
+        self.exit()
+        
+    def motor0(self,vel,robotid):
+        if(vel >= 0 and vel <= 100):
+            self.board.send_sysex(1,[vel, robotid])    
+
+    def motor1(self,vel,robotid):
+        if(vel >= 0 and vel <= 100):
+            self.board.send_sysex(2,[vel, robotid])   
+            
+    def motors(self,vel1, vel2, seconds=-1, robotid=0):
+        if(abs(vel1)<=100 and abs(vel2)<=100):
+            self.board.send_sysex(4,[abs(vel1), abs(vel2), 1 if vel1>0 else 0, 1 if vel2>0 else 0, robotid])   
+            if seconds!=-1:
+                self.board.pass_time(seconds)
+                self.motors(0,0,-1,robotid)
+
+    def ping(self,robotid):
+        self.board.send_sysex(3,[robotid])
+        # wait 20ms (ping delay) + 20ms (comm)
+        self.board.pass_time(0.04)
+        return self.board.nearest_obstacle[robotid]  
+
+    def sleep(self,time):
+        self.board.pass_time(time)
+
+    def exit(self):
+        self.board.exit()
+        
+    def analog(self,ch,samples=1,robotid=0): 
+        self.board.send_sysex(6,[ch, samples, robotid])
+        self.board.pass_time(0.02)
+        return self.board.analog_value[robotid]
+
+    def battery(self,robotid): 
+        self.board.send_sysex(6,[6, 1, robotid])
+        self.board.pass_time(0.02)
+        return self.board.analog_value[robotid]*5.0/1024
+
+    def digital(self,pin,robotid): 
+        self.board.send_sysex(7,[pin,robotid])
+        self.board.pass_time(0.02)
+        return self.board.digital_value[robotid]
+        
+    def tone(self, freq=0, seconds=0, robotid=0):
+        hi=freq>>7
+        lo=freq%128
+        if freq!=0:
+            if duration==0:
+                self.board.send_sysex(5,[hi, lo, robotid])
+            else:
+                self.board.send_sysex(5,[hi, lo, int(seconds*1000), robotid])
+                self.board.pass_time(seconds)
+
+        else:
+            self.board.send_sysex(5,[robotid])
+
+    def report(self): 
+    	self.board.live_robots=[0 for i in range(128)]
+        self.board.send_sysex(9,[])
+        self.board.pass_time(0.5)
+        for i in range(128):
+            if self.board.live_robots[i]:
+                print "Robot", i, "prendido"
+
+    def set_id(self,new_id,robotid): 
+        self.board.send_sysex(8,[new_id,robotid])
+        self.board.pass_time(0.02)
+    
+    def getLine(self, robotid):
+        a = self.analog(18)
+        b = self.analog(19)
+        return (a,b)
+
+class Robot:
+    def __init__(self, board, robotid=0):
+        '''Inicializa el robot y lo asocia con la placa board'''
+        self.robotid = robotid
+        self.board = board
+        self.name = ''
+
+    ##MOVIMIENTO
+    def forward(self, vel=50, seconds=-1):
+        '''El robot avanza con vel impulse durante seconds seconds.'''
+        self.board.motors(-(vel), -(vel), seconds, self.robotid)
+
+    def backward(self, vel=-50, seconds=-1):
+        '''El robot retrocede con velocidad vel durante seconds segundos.'''
+        self.forward(impulse,seconds)
+    
+    def turnLeft(self, vel=50, seconds=-1):
+       '''El robot gira a la izquierda con velocidad vel durante seconds segundos.'''
+       self.board.motors(vel, 0, seconds, self.robotid)
+
+    def turnRight(self, vel=50, seconds=-1):
+        '''El robot gira a la derecha con velocidad vel durante seconds segundos.'''
+        self.board.motors(0, vel, seconds, self.robotid)
+
+    def stop(self):
+        '''Detiene todo movimiento del robot inmediatamente'''
+        self.board.motors(0, 0, robotid=self.robotid)
+
+    ##SENSORES
+    def getLine(self):
+        '''Devuelve los valores de los sensores de Line.'''
+        return self.board.getLine(self.robotid)
+
+    def getObstacle(self, distance=10):
+        '''Devuelve True si hay un obstaculo a menos de distance centimetros del robot.'''
+        if self.board.ping(self.robotid) < distance :
+            return True
+        else:
+            return False
+
+    def battery(self):
+        '''Devuelve el voltaje de las baterías del robot.'''
+        self.board.battery(self.robotid)
+        self.board.pass_time(0.02)
+        return self.board.analog_value*5.0/1024
+
+    def sensors(self):
+        '''Imprime informacion de los sensores.'''
+        print 'Line1 = ' + str(self.analog(18))
+        print 'Line2 = ' + str(self.analog(19))
+        print 'Obstaculo mas cercano = ' + str(self.ping()) + ' cm.'
+        print 'Pontenciometro = ' + str(self.analog(15))
+        print 'Bateria = ' + str(self.battery()) + ' v.'
+    
+    ##Identificadores
+    def setid(self, newid):
+        '''Setea el robotid'''
+        self.board(newid, self.robotid)
+        self.robotid = newid
+    
+    def setname(self, name):
+        '''Setea el nombre para el robot.'''
+        self.name = str(name)
+    
+    def getid(self):
+        '''Devuelve el robotid.'''
+        return self.robotid
+
+    def getname(self):
+        '''Devuelve el nombre del robot.'''
+        return self.name
+
+    
+
