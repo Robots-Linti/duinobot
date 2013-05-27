ace2e2f3c2219c015260b0e717bc459dd78eb993
Fernando López <fernando.e.lopez@gmail.com>
2013-05-27 19:22:18 -0300

    Pseudo video

--- images/camera.py
+++ images/camera.py
@@ -4,37 +4,62 @@
 import cv
 import Tkinter as tk
 import ImageTk
+import PIL
+import PIL.Image
 
 __window = 0
+
+def __ipl2pil(frame):
+	return PIL.Image.fromstring(
+		'RGB', 
+		cv.GetSize(frame), 
+		frame.tostring(), 
+		'raw', 
+		'BGR', 
+		frame.width*3, 
+		0
+	)
 
 def takePicture(index = -1):
 	capture = cv.CaptureFromCAM(index)
 	frame = cv.QueryFrame(capture)
-	#cv.ReleaseCapture()
-	return frame
-
-def showCV(image):
-	global __window
-	__window += 1
-	id = "Ventana " + str(__window)
-	cv.NamedWindow(id)
-	print image
-	#print "Viene el show"
-	cv.ShowImage(id, cv.GetImage(image))
-	cv.WaitKey(-1)
+	return __ipl2pil(frame)
 
 def show(image):
-	window = tk.Tk()
-	window.grid()
-	#frame = tk.Frame()
-	#frame.master.title("Imagen")
-	#frame.grid()
+	#window = tk.Tk()
+	#window.grid()
+	window = root
 	photo = ImageTk.PhotoImage(image)
-	p = photo
-	label = tk.Label(image=photo)
+	label = tk.Label(window, image=photo)
 	label.grid()
-	tk.mainloop()
-	#frame.mainloop()
+	#tk.mainloop()
 
-if __name__ == "__main__":
-	show(takePicture())
+
+import threading
+__rootLock = threading.Lock()
+def __guiLoop():
+	global root
+	root = tk.Tk()
+	root.grid()
+	label = None
+	__rootLock.release()
+	while True:
+		__rootLock.acquire()
+		root.update()
+		image = takePicture()
+		photo = ImageTk.PhotoImage(image)
+		if label:
+			label.destroy()
+		label = tk.Label(root, image=photo)
+		label.grid()
+		__rootLock.release()
+
+__rootLock.acquire()
+__guiThread = threading.Thread(target=__guiLoop)
+__guiThread.start()
+#while True:
+#	__rootLock.acquire()
+#	show(takePicture())
+#	__rootLock.release()
+
+
