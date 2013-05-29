f23007d38a3ec7eeaf53ebbf8ea98b4fbfadd940
Fernando López <fernando.e.lopez@gmail.com>
2013-05-29 18:26:41 -0300

    Video fluido funcionando

--- images/square_wheel.py
+++ images/square_wheel.py
@@ -6,8 +6,12 @@ import atexit
 import time
 import Queue as queue
 import Tkinter as tk
-
+import cv
 import signal
+import copy
+import PIL
+import PIL.Image
+import ImageTk
 
 _terminate_thread = threading.Event()
 
@@ -34,7 +38,8 @@ def _tk_loop_cleanup():
 	if _tk_loop_thread.is_alive():
 		print "Timeout :("
 # También para el Ctrl+C:
-signal.signal(signal.SIGINT, lambda signal, frame: _tk_loop_cleanup)
+signal.signal(signal.SIGINT | signal.SIGTERM, lambda signal, frame: _tk_loop_cleanup)
+#signal.signal(signal.SIGTERM, lambda signal, frame: _tk_loop_cleanup)
 
 _tk_loop_thread = threading.Thread(target=_tk_loop)
 #print _tk_loop_thread.daemon
@@ -46,7 +51,7 @@ _request_queue = queue.Queue()
 def _tk_attend_requests():
 	"""Atiende requests desde adentro del thread,
 	recibe métodos desde _request_queue"""
-	if _terminate_thread.is_set():
+	if _terminate_thread == None or _terminate_thread.is_set():
 		# Terminar el thread desde adentro
 		_tk_root.quit()
 		exit()
@@ -68,7 +73,61 @@ class Senses(object):
 		label = tk.Label(toplevel, text="Hola")
 		label.pack()
 
-a = raw_input()
+def _ipl2pil(frame):
+        return PIL.Image.fromstring(
+                'RGB', 
+                cv.GetSize(frame), 
+                frame.tostring(), 
+                'raw', 
+                'BGR', 
+                frame.width*3, 
+                0   
+        )
+
+
+class WebCam(object):
+	def __init__(self, index = 0):
+		self.index = index
+		self.capture = cv.CaptureFromCAM(self.index)
+	def takePicture(self):
+		return Picture(_ipl2pil(cv.QueryFrame(self.capture)))
+
+showWindow = None
+
+class Picture(object):
+	def __init__(self, pilimage):
+		self.image = pilimage
+	def show(self):
+		def doShow():
+			global showWindow, showLabel
+			if not showWindow:
+				showWindow = tk.Toplevel()
+				showWindow.grid()
+				showLabel = tk.Label(showWindow)
+        			showLabel.pack()
+				showLabel.photoref = None
+			# Variable de instancia para evitar al Garbage Collector (es un bug de ImageTk)
+			if not showLabel.photoref:
+				showLabel.photoref =  ImageTk.PhotoImage(self.image)
+				showLabel.configure(image = showLabel.photoref)
+			else:
+				showLabel.photoref.paste(self.image)
+		_request_queue.put(doShow)
+		# Para darle una chance de ejecutarse al otro thread
+		# hay que hacer una operación bloqueante:
+		time.sleep(.1)
+
+#a = raw_input()
 s = Senses(2)
-b = raw_input()
-	
+w = WebCam()
+for i in xrange(100):
+	picture = w.takePicture()
+	picture.show()
+#b = raw_input()
+#try:
+#	while True:
+#		time.sleep(1)
+#except KeyboardInterrupt:
+#	pass
+
+time.sleep(5)
