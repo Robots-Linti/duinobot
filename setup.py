e166eea5c56d13d39ca88a66f08877d0f33538b7
Fernando López <flopez@linti.unlp.edu.ar>
2013-09-02 13:38:48 -0300

    Versión corregida en setup.py

--- setup.py
+++ setup.py
@@ -5,12 +5,13 @@ import os.path
 import glob
 
 setup(name='duinobot',
-      version='0.10',
+      version='0.11',
       description='API en Python para el robot Multiplo N6',
       author='Joaquin Bogado, Fernando Lopez',
       author_email='soportelihuen@linti.unlp.edu.ar',
       url='http://lihuen.linti.unlp.edu.ar',
       packages=['duinobot', 'duinobot.pyfirmata'],
       package_dir={'duinobot': '.'},
-      data_files=[('/usr/share/doc/duinobot/ejemplos', glob.glob(os.path.join('ejemplos','*')))]
+      data_files=[('/usr/share/doc/duinobot/ejemplos', glob.glob(os.path.join('ejemplos','*')))],
+      requires=['pyserial', 'pygame']
      )
