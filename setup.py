f953fa994a8ff8e4bd4c66620dcc35b5fc1c1611
Fernando López <flopez@linti.unlp.edu.ar>
2013-12-12 13:42:59 -0300

    Empaquetado y listo para Pypi

--- setup.py
+++ setup.py
@@ -5,13 +5,23 @@ import os.path
 import glob
 
 setup(name='duinobot',
-      version='0.11-2',
+      version='0.12',
       description='API en Python para el robot Multiplo N6',
       author='Joaquin Bogado, Fernando Lopez',
       author_email='soportelihuen@linti.unlp.edu.ar',
       url='http://lihuen.linti.unlp.edu.ar',
       packages=['duinobot', 'duinobot.pyfirmata'],
       package_dir={'duinobot': '.'},
-      data_files=[('usr/share/doc/duinobot/ejemplos', glob.glob(os.path.join('ejemplos','*')))],
-      requires=['pyserial', 'pygame']
+      data_files=[('usr/share/doc/duinobot/ejemplos', glob.glob(os.path.join('ejemplos','*.py')))],
+      requires=['pyserial', 'pygame'],
+      classifiers = [
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Education",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7"
+       ]
      )
